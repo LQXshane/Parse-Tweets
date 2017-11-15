8a9cf78be2be180c2eeb6c8affeed2dcbbf6944c
LQXshane <qxlin@bu.edu>
2017-11-15 11:17:55 -0800

    re-count

--- main_counter.py
+++ main_counter.py
@@ -10,12 +10,16 @@ import sys
 reload(sys)
 sys.setdefaultencoding('UTF8')
 
+# from  2016-08-04 to  2016-10-23
+# Total 26,246,374 tweets
+
 if __name__ == '__main__':
     jsdir = sys.argv[1]
     utc = pytz.utc
     MAXDATE = datetime(2010,01,01,tzinfo=utc)
     MINDATE = datetime(2017,12,30,tzinfo=utc)
     COUNT = 0
+    INVALIDS = 0
     files = [ f for f in os.listdir(jsdir) if os.path.isfile(os.path.join(jsdir,f))]
     j = 0; print "%d of json files in total"%(len(files))
     for f in files:
@@ -29,12 +33,16 @@ if __name__ == '__main__':
             for i in range(len(data)):
                 try:
                     dt = parser.parse(data[i]['created_at'])
-                except: continue
+                except:
+                    # print "No valid datetime on record"
+                    INVALIDS += 1
+                    continue
                 if 'created_at' in data[i]:
                     if dt > MAXDATE:
                         MAXDATE = dt
                     if dt < MINDATE:
                         MINDATE = dt
 
-    print "from ",MINDATE.date(), "to ", MAXDATE.date()
+    print "From ",MINDATE.date(), "to ", MAXDATE.date()
     print "Total %d tweets"%(COUNT)
+    print "%d tweets does not have datetime"%(INVALIDS)
